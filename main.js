211aca8cfd35001c112799f711106d36f7b65622
Liu Gui <77035087+LiuTrof@users.noreply.github.com>
2024-07-14 15:48:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import { fetch, setGlobalDispatcher, Agent } from 'undici'
-import fetch from 'node-fetch';  // 使用 node-fetch 代替 undici
+import fetch from 'node-fetch';
 
 const testConnection = async () => {
   try {
@@ -13,49 +12,54 @@ const testConnection = async () => {
   }
 };
 
-testConnection();
-
-
-setGlobalDispatcher(new Agent({
-  connect: { timeout: 10000 } // 设置超时时间为 10 秒
-}))
+const retryFetch = async (url, options, retries = 3) => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) throw new Error('Network response was not ok');
+      return response;
+    } catch (error) {
+      if (i === retries - 1) throw error;
+    }
+  }
+};
 
 const glados = async () => {
-  const cookie = process.env.GLADOS
-  if (!cookie) return
+  const cookie = process.env.GLADOS;
+  if (!cookie) return;
   try {
     const headers = {
       'cookie': cookie,
       'referer': 'https://glados.rocks/console/checkin',
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
-    }
-    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
+    };
+    const checkin = await retryFetch('https://glados.rocks/api/user/checkin', {
       method: 'POST',
       headers: { ...headers, 'content-type': 'application/json' },
       body: '{"token":"glados.one"}',
-    }).then((r) => r.json())
-    const status = await fetch('https://glados.rocks/api/user/status', {
+    }).then((r) => r.json());
+    const status = await retryFetch('https://glados.rocks/api/user/status', {
       method: 'GET',
       headers,
-    }).then((r) => r.json())
+    }).then((r) => r.json());
     return [
       'Checkin OK',
       `${checkin.message}`,
       `Left Days ${Number(status.data.leftDays)}`,
-    ]
+    ];
   } catch (error) {
     return [
       'Checkin Error',
       `${error}`,
       `<${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}>`,
-    ]
+    ];
   }
-}
+};
 
 const notify = async (contents) => {
-  const token = process.env.NOTIFY
-  if (!token || !contents) return
-  await fetch(`https://www.pushplus.plus/send`, {
+  const token = process.env.NOTIFY;
+  if (!token || !contents) return;
+  await retryFetch(`https://www.pushplus.plus/send`, {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({
@@ -64,11 +68,12 @@ const notify = async (contents) => {
       content: contents.join('<br>'),
       template: 'markdown',
     }),
-  })
-}
+  });
+};
 
 const main = async () => {
-  await notify(await glados())
-}
+  await notify(await glados());
+};
 
-main()
+testConnection();
+main();
